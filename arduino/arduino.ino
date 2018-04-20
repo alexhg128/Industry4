84443786a47699471c8f46c515fd952c30fc91a8
Alejandro Hahn <30887815+alexhg128@users.noreply.github.com>
2018-04-20 15:57:45 -0600

    Add files via upload

---
+++ arduino/arduino.ino
@@ -0,0 +1,44 @@
+const int buttonPin = 3;
+const int ledPin =  2;
+const int ledPin2 = 4;
+const int buttonPin2 = 5;
+
+int buttonState = 0;
+int button2State = 0;
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(buttonPin2, INPUT);
+
+}
+
+void loop() {
+
+  buttonState = digitalRead(buttonPin);
+  button2State = digitalRead(buttonPin2);
+  
+  if (buttonState == HIGH && button2State == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    Serial.print("1");
+  } else if(buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin2, LOW);
+    Serial.print("2");
+  } else if(button2State == HIGH) {
+    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, HIGH);
+    Serial.print("3");
+  } else {
+    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, LOW);
+    Serial.print("0");
+  }
+
+  delay(100);
+
+}
